ca3af02e138d59c992f0c39bf04966df443fe2d2
luy <luylove77@163.com>
2025-05-21 19:20:51 +0800

    flink ttl

--- src/main/java/com/luy/flinksql/TTL_Time.java
+++ src/main/java/com/luy/flinksql/TTL_Time.java
@@ -4,7 +4,7 @@ import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
-// 设置Flink 状态保留时间，如果一直不删除的话，会一直占用内存，导致内存溢出
+// 设置Flink 状态保留时间(TTL)，如果一直不删除的话，会一直占用内存，导致内存溢出
 public class TTL_Time {
     public static void main(String[] args) {
         // 创建执行环境
